5b9ed22f9ad9e4b5a78e21f8d4ac8420c0f32c9d
Julie Headlee <julieheadlee@gmail.com>
2021-02-02 19:45:38 -0600

    Add sql file

---
+++ grammys.sql
@@ -0,0 +1,11 @@
+CREATE TABLE grammys (
+id INTEGER PRIMARY KEY,
+year INTEGER,
+category VARCHAR (50),
+nominee TEXT,
+artist VARCHAR (50),
+workers TEXT,
+winner BOOLEAN
+);
+
+select * from grammys;
