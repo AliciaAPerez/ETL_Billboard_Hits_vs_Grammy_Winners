a5e02347ee88491a68b7cf8bc5e3202e2d2015f9
Julie Headlee <julieheadlee@gmail.com>
2021-02-02 21:45:46 -0600

    Final project

--- Joins.sql
+++ Joins.sql
@@ -1,1 +1,9 @@
- 
+SELECT b.artists, b.song_title, b.genre, b.week, b.weeks_on_chart, g.year, g.category, g.winner, s.danceability, s.energy, s.popularity
+FROM billboard as b
+JOIN grammys as g ON
+g.nominee = b.song_title AND
+g.artist = b.artists
+JOIN spotify as s ON
+s.song_title = b.song_title AND
+s.artists=b.artists
+;
